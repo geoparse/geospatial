598d3b3ccff2d574e87fb81a911bf28b22c1e47a
Abbas Kiasari <kiasari@gmail.com>
2024-09-29 20:50:02 +0100

    Rewrite cell_to_geom function

--- geospatial/sindex.py
+++ geospatial/sindex.py
@@ -219,7 +219,35 @@ def geom_to_cell_parallel_2(mdf, cell_type, res, compact=False, verbose=False, d
     return
 
 
-def cell_to_geom(cells, cell_type):
+def cell_to_geom(cells: list, cell_type: str) -> tuple:
+    """
+    Converts a list of spatial cells to their corresponding geometries and resolution levels.
+
+    The function takes a list of spatial cells (e.g., Geohash, H3, or S2) and converts each cell
+    into a geometry object (Polygon) based on the specified cell type. It also calculates the resolution
+    level for each cell.
+
+    Parameters:
+    ----------
+    cells : list
+        A list of spatial cells represented as strings. Each cell corresponds to a spatial area
+        in a specific grid system (e.g., Geohash, H3, or S2).
+
+    cell_type : str
+        The type of spatial cell system used. Accepted values are:
+        - "geohash" : Geohash spatial indexing system.
+        - "h3"      : H3 hexagonal spatial indexing system.
+        - "s2"      : S2 spherical spatial indexing system.
+
+    Returns:
+    -------
+    tuple
+        A tuple containing:
+        - `res` : list of int
+            A list of resolution levels corresponding to each cell in the input.
+        - `geoms` : list of shapely.geometry.Polygon
+            A list of Polygon geometries representing the spatial boundaries of the input cells.
+    """
     res = [
         len(cell)
         if cell_type == "geohash"
