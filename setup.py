b53a405a53355c5553f9bd4c80cffdce7c4d0811
Abbas Kiasari <kiasari@gmail.com>
2024-10-15 21:35:40 +0100

    Change madule name in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="geospatial",
+    name="geoparse",
     version="0.2.0",
-    python_requires=">=3.6",
-    packages=["geospatial"],
+    python_requires=">=3.10",
+    packages=["geoparse"],
 )
